eaa799862f28951077bea5402e9cecf8af68cda6
OleksandKur <136488587+OleksandrKur@users.noreply.github.com>
2024-04-02 19:09:52 +0300

    Add functionality to calculator

--- calculator.js
+++ calculator.js
@@ -1,34 +1,54 @@
-const numberButtons =  document.querySelectorAll(".number")
+const MINUSDECIMALPLACES = 1000;
 const currentNumberDisplay = document.querySelector("#current-number");
+
+const numberButtons =  document.querySelectorAll(".number");
+const sign = document.querySelector("#sign");
+
+const operationButtons = document.querySelectorAll(".operation")
+const resultButton = document.querySelector("#result");
+
 const deleteButton = document.querySelector("#del");
 const clearButton = document.querySelector("#clear");
-const sign = document.querySelector("#sign");
 const clearAllButton = document.querySelector("#clear-all");
-let currentNumber = "";
+
+let currentNumber = "0";
+let mathOperation = "";
+let result = "0";
+currentNumberDisplay.textContent = currentNumber;
 
 
 numberButtons.forEach(number => {
     number.addEventListener("click", updateNumber)
 });
-
 deleteButton.addEventListener("click", deleteDigit);
 clearButton.addEventListener("click", deleteNumber);
+clearAllButton.addEventListener("click", deleteAll);
 sign.addEventListener("click", changeSign);
 
+operationButtons.forEach(operationButton => {
+    operationButton.addEventListener("click", updateOperation);
+});
+
+resultButton.addEventListener("click", resultButtonAction);
+
+
+
 function add(a, b){
-    return a + b;
+    return Math.round((a + b) * MINUSDECIMALPLACES) / MINUSDECIMALPLACES;
 }
-
 function subtract(a, b){
-    return a - b;
+    return Math.round((a - b) * MINUSDECIMALPLACES) / MINUSDECIMALPLACES;
 }
-
 function multiply(a, b){
-    return a * b;
+    return Math.round((a * b) * MINUSDECIMALPLACES) / MINUSDECIMALPLACES;
 }
-
 function divide(a, b){
-    return a / b;
+    if (b !== 0){
+        return Math.round((a / b) * MINUSDECIMALPLACES) / MINUSDECIMALPLACES;
+    }
+    else{
+        return "It's impossible to divide by 0";
+    }
 }
 
 function operate(a, b, operator){
@@ -50,35 +70,112 @@ function operate(a, b, operator){
 }
 
 function getOperandsAndOperator(str){
-    [a, operator, b] = str.split(" ")
-    a = Number(a);
-    b = Number(b);
-    return [a, operator, b]
+    let arrayFromString = str.split(" ");
+    let a;
+    let b;
+    let operator;
+    if (arrayFromString.length < 3){
+        a = Number(arrayFromString[0]);
+        b = 0;
+        operator = "+";
+    }
+    else{
+        a = Number(arrayFromString[0]);
+        b = Number(arrayFromString[2]);
+        operator = arrayFromString[1];
+        
+    }
+    let outputArray = [a, b, operator]
+    return outputArray;
 }
 
 function calculatePair(string){
-    [a, operator, b] = getOperandsAndOperator(example);
-    return operate(a, b, operator);
+    return operate(...getOperandsAndOperator(string));
 }
 
 function updateNumber(event){
-    if(event.target.textContent === "." && currentNumber.includes(".")){
+    let buttonValue = event.target.textContent;
+    if (currentNumber === result){
+        currentNumber = "0";
+    }
+    if (buttonValue === "0" && currentNumber === "0"){
         return
     }
-    currentNumber = currentNumber + event.target.textContent;
-    return currentNumberDisplay.textContent = currentNumber;
+    if (buttonValue === "." && currentNumber.includes(".")){
+        return
+    }
+    if (buttonValue !== "." && currentNumber === "0" ){
+        currentNumber = buttonValue;
+        updateDisplay(currentNumber);
+        return
+    }
+    
+    currentNumber = currentNumber + buttonValue;
+    updateDisplay(currentNumber);
 }
+
+function updateOperation(event){
+    if (currentNumber == "It's impossible to divide by 0"){
+        return;
+    }
+    let operator = " " + event.target.textContent + " ";
+    if (mathOperation === ""){
+        mathOperation = currentNumber + operator;
+        updateDisplay(currentNumber);
+        currentNumber = "0"
+        
+    }
+    else{
+        mathOperation = mathOperation + currentNumber;
+        result = calculatePair(mathOperation);
+        mathOperation = "";
+        currentNumber = result;
+        if (currentNumber == "It's impossible to divide by 0"){
+            updateDisplay(result);
+            return;
+        }
+        mathOperation = currentNumber + operator;
+        currentNumber = "0";
+        updateDisplay(result);
+    }
+}
+
+function resultButtonAction(){
+    mathOperation = mathOperation + currentNumber;
+    result = calculatePair(mathOperation);
+    mathOperation = "";
+    currentNumber = result;
+    updateDisplay(result);
+}
+
 
 function deleteNumber(){
-    currentNumber = "";
-    currentNumberDisplay.textContent = currentNumber;
-}
-function deleteDigit(){
-    currentNumber = currentNumber.substring(0, currentNumber.length - 1);
-    currentNumberDisplay.textContent = currentNumber;
+    currentNumber = "0";
+    updateDisplay(currentNumber);
 }
 
+function deleteAll(){
+    currentNumber = "0";
+    updateDisplay(currentNumber);
+    mathOperation = "";
+}
+function deleteDigit(){
+    if(currentNumber.length > 1){
+        currentNumber = currentNumber.substring(0, currentNumber.length - 1);
+        updateDisplay(currentNumber);
+    }
+    else{
+        currentNumber = "0"
+        updateDisplay(currentNumber);
+    }
+    
+    
+}
 function changeSign(){
     currentNumber = (Number(currentNumber) * -1) + "";
     currentNumberDisplay.textContent = currentNumber;
+}
+
+function updateDisplay(text){
+    currentNumberDisplay.textContent = text;
 }
